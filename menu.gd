e485f7d9fba86f6befc1a27184a924f2a55aee5a
akosior <01206558@pw.edu.pl>
2026-06-12 00:52:37 +0200

    GIGA GOTOWANIE WIZUALNE I EASTER EGG UFKO

--- menu.gd
+++ menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+const UFO_IMG = preload("res://ufo.png")
+
 func _ready() -> void:
 	# Kiedy menu się załaduje, podpinamy akcje pod kliknięcia (pressed)
 	$VBoxContainer/Start.pressed.connect(kliknieto_start)
@@ -8,7 +10,7 @@ func _ready() -> void:
 
 func kliknieto_start() -> void:
 	# Odpalamy Twój główny poziom (zmień nazwę pliku, jeśli zapisałeś go inaczej!)
-	get_tree().change_scene_to_file("res://label.tscn")
+	get_tree().change_scene_to_file("res://poziom_1.tscn")
 
 func kliknieto_poziomy() -> void:
 	# Na razie wyświetlamy tylko tekst w konsoli na dole ekranu
@@ -17,3 +19,37 @@ func kliknieto_poziomy() -> void:
 func kliknieto_wyjdz() -> void:
 	# Bezpiecznie zamykamy całą grę
 	get_tree().quit()
+
+func _on_ufo_timer_timeout() -> void:
+	# randf() losuje ułamek od 0.0 do 1.0. 0.01 to jest dokładnie 1% szansy
+	if randf() <= 0.05:
+		spawn_ufo()
+
+func spawn_ufo() -> void:
+	print("UFO is sashaying across the screen!")
+	var ufo = Sprite2D.new()
+	ufo.texture = UFO_IMG
+	ufo.scale = Vector2(0.5,0.5)
+	
+	ufo.z_index=100
+	add_child(ufo)
+	
+	var screen_size = get_viewport_rect().size
+	
+	# Losujemy stronę: true = lewa, false = prawa
+	var leci_z_lewej = randi() % 2 == 0
+	
+	# Ustawiamy X w zależności od wylosowanej strony
+	var start_x = -200 if leci_z_lewej else screen_size.x + 200
+	var end_x = screen_size.x + 200 if leci_z_lewej else -200
+	
+	# Losujemy wysokość Y totalnie niezależnie dla startu i mety
+	var start_y = randf_range(100, screen_size.y - 100)
+	var end_y = randf_range(100, screen_size.y - 100)
+	
+	ufo.global_position = Vector2(start_x, start_y)
+	
+	# Płynny lot do nowej pozycji
+	var tween = create_tween()
+	tween.tween_property(ufo, "global_position", Vector2(end_x, end_y), 4.0)
+	tween.tween_callback(ufo.queue_free)
--- stacja.gd
+++ stacja.gd
@@ -1,10 +1,26 @@
-extends Sprite2D
+extends Node2D 
+
+@export var nastepny_poziom: PackedScene
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	# Sprawdzamy czy ciało, które w nas uderzyło, to faktycznie rakieta
 	if body.name == "Rakieta":
-		print("Zadokowano")
+		print("Zadokowano, the end of an era.")
 		body.w_locie = false
 		body.zadokowana = true 
 		body.predkosc = Vector2.ZERO
 		body.global_position = global_position
+		
+		# Odpalamy confetti
+		if has_node("Confetti"):
+			$Confetti.emitting = true
+			
+	# Pokazujemy cala ramke z tekstem
+		if has_node("CanvasLayer/Ramka"):
+			$"CanvasLayer/Ramka".show()
+			
+		# Zatrzymujemy kod na 2 sekundy, zeby podziwiac ten show
+		await get_tree().create_timer(2.0).timeout
+		
+		# Dopiero po czasie zmieniamy poziom
+		if nastepny_poziom:
+			get_tree().change_scene_to_packed(nastepny_poziom)
